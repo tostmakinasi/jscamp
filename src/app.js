34af6c1fd3d9ab21f202c866fec950c163cfa043
Kubilay Cem <kubilaycem@yandex.com>
2021-10-19 19:15:18 +0300

    first lesson 18.10.21

---
+++ src/app.js
@@ -0,0 +1,14 @@
+console.log("Merhaba Kodlama.io")
+
+let dolarDun= 9.20
+let dolarBugun = 9.30
+
+let konutKredileri = ["Konut Kredisi","Emlak Konut Kredisi","Kamu Konnut Kredisi"]
+
+console.log("<ul>")
+for (let i = 0; i < konutKredileri.length; i++) {
+    console.log("<il>"+konutKredileri[i]+"</il>")
+}
+console.log("</ul>")
+
+console.log(konutKredileri)
