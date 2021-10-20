901600e3bb102392706eb266406db8bd50204893
Kubilay Cem <kubilaycem@yandex.com>
2021-10-20 23:36:16 +0300

    1.ÖdevSonu

--- src/app.js
+++ src/app.js
@@ -1,14 +1,68 @@
-console.log("Merhaba Kodlama.io")
+// console.log("Merhaba Kodlama.io")
 
-let dolarDun= 9.20
-let dolarBugun = 9.30
+import { Customer } from "./oop"
 
-let konutKredileri = ["Konut Kredisi","Emlak Konut Kredisi","Kamu Konnut Kredisi"]
+// let dolarDun= 9.20
+// let dolarBugun = 9.30
 
-console.log("<ul>")
-for (let i = 0; i < konutKredileri.length; i++) {
-    console.log("<il>"+konutKredileri[i]+"</il>")
-}
-console.log("</ul>")
+// let konutKredileri = ["Konut Kredisi","Emlak Konut Kredisi","Kamu Konnut Kredisi"]
 
-console.log(konutKredileri);
+// console.log("<ul>")
+// for (let i = 0; i < konutKredileri.length; i++) {
+//     console.log("<il>"+konutKredileri[i]+"</il>")
+// }
+// console.log("</ul>")
+
+// console.log(konutKredileri);
+
+// let student = {id:1,name:"Kubilay", puan : null}
+
+//console.log(student)
+
+// function save(deger, puan) {
+    // console.log(deger.name +" : "+ puan )
+// }
+
+//save(student,40)
+
+// let students = ["Kubilay","Halit","Cem"]
+
+//console.log(students)
+
+// let students2 = [student,{id:2,name:"Halit", puan : null},"Ankara",{city :"Istanbul"}]
+
+//console.log(students2)
+//rest
+//params
+// let showProducts = function (id,...products){
+    // console.log(id)
+    // console.log(products)
+// }
+
+// console.log(typeof showProducts)
+// showProducts(5,"bir","iki","üç")
+//SPREAD
+let points = [5,6,77,8899,6]
+
+console.log(Math.max(...points))
+console.log(...points)
+
+console.log(..."ABC","D",..."EFG","H")
+
+//Destructuring
+let populations = [10000,20000,30000]
+
+let [small,medium,high] = populations
+
+// console.log(small)
+// console.log(medium)
+// console.log(high)
+
+let category = {id: 1, name: "Ýçecek"}
+
+let {id,name} = category
+
+console.log(name)
+console.log(id)
+
+//
---
+++ src/oop/index.js
@@ -0,0 +1,36 @@
+import { populations } from "../app"
+
+export class Customer{
+    constructor(id,customerNumber){
+        this.id = id
+        this.customerNumber = customerNumber
+    }
+}
+
+let customer = new Customer(1,12345)
+
+customer.name = "Kubilay Cem"
+
+console.log(customer.id)
+
+Customer.bisey ="biþeyler"
+
+console.log(customer.bisey)
+
+
+class IndividualCustomer extends Customer {
+
+    constructor(fistname, id, customerNumber){
+        super(id,customerNumber)
+        this.fistname = fistname
+    }
+}
+
+class CorporateCustomer extends Customer{
+    constructor(companyName, id, customerNumber){
+        super(id,customerNumber)
+        this.companyName = companyName
+    }
+}
+
+console.log(populations.)
